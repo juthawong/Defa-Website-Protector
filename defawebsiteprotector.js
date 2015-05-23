0f1bfd029950e5037dd948741465006fd6a46d17
Juthawong Naisanguansee <juthawong@users.noreply.github.com>
2015-05-23 20:34:07 +0700

    Update defawebsiteprotector.js

--- defawebsiteprotector.js
+++ defawebsiteprotector.js
@@ -8,8 +8,17 @@ i = jQuery('html').html();
 
 setInterval(function(){defacheck3()
 },1000)
+var defacheck = 0;
+$(document.body).keypress(function(event){
+var defacheck = 1;
+
+}
+$(document.body).keyup(function(){
+var defacheck = 0
+
+}
 function defacheck3(){
- if(i !== jQuery('html').html()){
+ if(i !== jQuery('html').html() && defacheck === 0){
 window.location.href="http://www.juthawong.com/redirect.php?r="+window.location;
 }
 }
