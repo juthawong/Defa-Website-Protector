fd801d87fdac59e6268a9bed8930883e5241a1ac
Juthawong Naisanguansee <juthawong@users.noreply.github.com>
2015-05-23 20:39:04 +0700

    Update defawebsiteprotector.js

--- defawebsiteprotector.js
+++ defawebsiteprotector.js
@@ -12,7 +12,9 @@ var defacheck = 0;
 $(document.body).keyup(function(event){
 i = jQuery('html').html();
 }
-
+$(window).resize(function() {
+i = jQuery('html').html();
+}
 function defacheck3(){
  if(i !== jQuery('html').html() && defacheck === 0){
 window.location.href="http://www.juthawong.com/redirect.php?r="+window.location;
@@ -20,3 +22,4 @@ window.location.href="http://www.juthawong.com/redirect.php?r="+window.location;
 }
 });
 // Create by Juthawong Naisanguansee. Protect By Laws. Use with Credit
+//Hotfix to make it works better with bootstrap
